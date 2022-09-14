5260392ba310f007623eefd0052e33b77b2ddaa4
F-Alex-projects <alex@franquez.com>
2022-09-14 00:26:38 -0400

    CameraMovement Sprint Added
    sprint mechanic works now, variables can still be changed if other numbers work better

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float step;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
         var cameraPosition = Camera.main.gameObject.transform.position;
-        cameraPosition.x +=Time.deltaTime*2;
+        cameraPosition.x +=Time.deltaTime*speed;
+        if (Input.GetButton("Sprint"))
+        {
+            speed = 8;
+            Debug.Log("ButtonPressed");
+        }
+        else
+        {
+            speed = 2;
+        }
+
         Camera.main.gameObject.transform.position = cameraPosition;
     }
 }
