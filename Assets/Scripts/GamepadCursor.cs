edc0cd52ee5565a753c0f7531ac27a6b9205e8fb
F-Alex-projects <alex@franquez.com>
2022-09-29 01:59:26 -0400

    Cursor works on UI
    Crosshair still does not actually shoot, but it can interact with buttons in both the main menu and the pause menu. Control scheme currently set to change depending on which input you are using, but I'll have to make the crosshair visible in both.

--- Assets/Scripts/GamepadCursor.cs
+++ Assets/Scripts/GamepadCursor.cs
@@ -15,14 +15,22 @@ public class GamepadCursor : MonoBehaviour
     private RectTransform canvasRectTransform;
     [SerializeField]
     private float cursorSpeed = 1000f;
+    [SerializeField]
+    private float padding = 35f;
     
     private bool previousMouseState;
     private Mouse virtualMouse;
+    private Mouse currentMouse;
     private Camera mainCamera;
+
+    private string previousControlScheme = "";
+    private const string gamepadScheme = "Gamepad";
+    private const string mouseScheme = "Keyboard&Mouse";
 
     private void OnEnable() 
     {
         mainCamera = Camera.main;
+        currentMouse = Mouse.current;
         if(virtualMouse == null)
         {
             virtualMouse = (Mouse) InputSystem.AddDevice("VirtualMouse");
@@ -41,11 +49,14 @@ public class GamepadCursor : MonoBehaviour
         }
         
         InputSystem.onAfterUpdate += UpdateMotion;
+        playerInput.onControlsChanged += OnControlsChanged;
     }
 
     private void OnDisable()
     {
+        if (virtualMouse != null && virtualMouse.added) InputSystem.RemoveDevice(virtualMouse);
         InputSystem.onAfterUpdate -= UpdateMotion;
+        playerInput.onControlsChanged -= OnControlsChanged;
     }
     
     private void UpdateMotion()
@@ -61,8 +72,8 @@ public class GamepadCursor : MonoBehaviour
         Vector2 currentPosition = virtualMouse.position.ReadValue();
         Vector2 newPosition = currentPosition + stickValue;
 
-        newPosition.x = Mathf.Clamp(newPosition.x, 0, Screen.width);
-        newPosition.y = Mathf.Clamp(newPosition.y, 0, Screen.height);
+        newPosition.x = Mathf.Clamp(newPosition.x, padding, Screen.width - padding);
+        newPosition.y = Mathf.Clamp(newPosition.y, padding, Screen.height - padding);
 
         InputState.Change(virtualMouse.position, newPosition);
         InputState.Change(virtualMouse.delta, stickValue);
@@ -85,5 +96,33 @@ public class GamepadCursor : MonoBehaviour
         RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectTransform, position, canvas.renderMode
             == RenderMode.ScreenSpaceOverlay ? null : mainCamera, out anchoredPosition);
         cursorTransform.anchoredPosition = anchoredPosition;
+    }
+
+    private void OnControlsChanged(PlayerInput input)
+    {
+        if (playerInput.currentControlScheme == mouseScheme && previousControlScheme != mouseScheme)
+        {
+            cursorTransform.gameObject.SetActive(false);
+            Cursor.visible = true;
+            currentMouse.WarpCursorPosition(virtualMouse.position.ReadValue());
+            previousControlScheme = mouseScheme;
+        }
+        else if (playerInput.currentControlScheme == gamepadScheme && previousControlScheme != gamepadScheme)
+        {
+            cursorTransform.gameObject.SetActive(true);
+            Cursor.visible = false;
+            InputState.Change(virtualMouse.position, currentMouse.position.ReadValue());
+            AnchorCursor(currentMouse.position.ReadValue());
+            previousControlScheme = gamepadScheme;
+        }
+    }
+
+    private void Update()
+    {
+        if (previousControlScheme != playerInput.currentControlScheme)
+        {
+            OnControlsChanged(playerInput);
+        }
+        previousControlScheme = playerInput.currentControlScheme;
     }
 }
